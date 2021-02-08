da7cd08fe09993a86151f0d8c1f94a768b5e466a
kushal9291-monoxor <kushal.parikh@monoxor.com>
2021-02-08 16:23:39 +0000

    sync

--- mxr-web/apps/proximity/src/App.js
+++ mxr-web/apps/proximity/src/App.js
@@ -6,21 +6,19 @@ import Home from '/mxr-web/apps/proximity/src/pages/Home/Home.react'
 import VirtualServices from '/mxr-web/apps/proximity/src/pages/virtual-services'
 import Policies from '/mxr-web/apps/proximity/src/pages/policies'
 
-class SRS extends Component {
-  render() {
-    return (
-      <AdminLayout>
-        <Switch>
-          <Route
+const SRS = () => {
+  return (
+    <AdminLayout>
+      <Switch>
+        {/* <Route
             path='/proximity/virtual-services'
             component={VirtualServices}
           />
-          <Route path='/proximity/policies' component={Policies} />
-          <Route path='/proximity' component={Home} />
-        </Switch>
-      </AdminLayout>
-    )
-  }
+          <Route path='/proximity/policies' component={Policies} /> */}
+        <Route path='/proximity' component={Home} />
+      </Switch>
+    </AdminLayout>
+  )
 }
 
 export default observer(SRS)
